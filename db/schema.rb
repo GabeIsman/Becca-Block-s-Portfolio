d94fe10d8ec0c271e886266a2ca72f50d5972176
Gabe Isman <gabe.isman@gmail.com>
2012-02-13 11:31:12 -0500

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120208192855) do
+ActiveRecord::Schema.define(:version => 20120208204049) do
 
   create_table "designs", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "title"
+    t.text     "info"
   end
 
   create_table "images", :force => true do |t|
